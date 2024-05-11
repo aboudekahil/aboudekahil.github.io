9b2e8c1b3dfd77ea4bd1d0a3743d84f698079e5c
aboude <aboudeh003@gmail.com>
2024-05-11 23:08:25 +0300

    prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,54 @@
-import { Navbar } from "./components/Navbar.tsx";
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa6";
 
 function App() {
   return (
-    <>
-      <Navbar></Navbar>
-    </>
+    <main className="p-4">
+      <section>
+        <h1>- Who am i</h1>
+        <h1 className="text-2xl shadow-2xl font-extrabold font-mono p-4">
+          Abd El Kader Kahil
+        </h1>
+        <h1 className="text-2xl shadow-2xl font-extrabold font-mono p-4">
+          Software developer
+        </h1>
+        <h2 className="text-2xl shadow-2xl font-extrabold font-mono p-4">
+          Bs. Computer science
+        </h2>
+      </section>
+
+      <section>
+        <h1>- My languages</h1>
+        <ul className="flex flex-wrap w-full gap-5 font-mono font-extrabold p-4">
+          <li>c++</li>
+          <li>java</li>
+          <li>c</li>
+          <li>python</li>
+          <li>javascript</li>
+          <li>typescript</li>
+          <li>golang</li>
+        </ul>
+      </section>
+      <section>
+        <h1>- links</h1>
+        <ul className="font-mono font-extrabold p-4 flex flex-col gap-5">
+          <a href="https://github.com/aboudekahil">
+            <li className="hover:cursor-[url(''), auto]">
+              github <FaGithub className="inline" />
+            </li>
+          </a>
+          <a href="https://linkedin.com/in/aboude">
+            <li className="hover:cursor-[url(''), auto]">
+              linkedin <FaLinkedin className="inline" />
+            </li>
+          </a>
+          <a href="mailto:aboudekahil@gmail.com">
+            <li className="hover:cursor-[url(''), auto]">
+              email <FaEnvelope className="inline" />
+            </li>
+          </a>
+        </ul>
+      </section>
+    </main>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,25 @@
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa6";
+
 export function Navbar() {
   return (
-    <nav className="text-white bg-gray-800 p-5 w-full flex justify-between items-center">
+    <nav className="bg-transparent backdrop-blur shadow-2xl bg-gray-800 p-5 w-full flex justify-between items-center">
       <h1 className="text-lg font-extrabold font-mono">Abd El Kader Kahil</h1>
-      <div className="hidden sm:block font-mono font-extrabold">
+      <div className="text-xl font-mono font-extrabold">
         <ul className="flex w-full gap-5">
           <li>
-            <a href="https://github.com/aboudekahil">github</a>
+            <a href="https://github.com/aboudekahil">
+              <FaGithub />
+            </a>
           </li>
           <li>
-            <a href="mailto:aboudehkahil@gmail.com">email</a>
+            <a href="mailto:aboudehkahil@gmail.com">
+              <FaEnvelope />
+            </a>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/aboude/">linked in</a>
+            <a href="https://www.linkedin.com/in/aboude/">
+              <FaLinkedin />
+            </a>
           </li>
         </ul>
       </div>
